31aadff02dc401c8453f3399e0f688c7dd3b5733
Leon Tran <leontran@LeonMBA.local>
2017-04-04 05:06:56 +0200

    database collection fixed2

--- src/models/users/decorators.py
+++ src/models/users/decorators.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from flask import session, request, redirect, url_for
-from src.app import app
+
+import src
 
 
 def requires_login(func):
@@ -18,7 +19,7 @@ def requires_admin_permissions(func):
     def decorated_function(*args, **kwargs):
         if 'email' not in session.keys() or session['email'] is None:
             return redirect(url_for('users.login_user', next=request.path))
-        if session['email'] not in app.config['ADMINS']:
+        if session['email'] not in src.config['ADMINS']:
             return redirect(url_for('users.login_user'))
         return func(*args, **kwargs)
 
