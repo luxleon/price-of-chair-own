e97777002de1a7a17098112538f5be0a122f1fec
Leon Tran <leontran@LeonMBA.local>
2017-04-04 05:22:53 +0200

    test fix3

--- src/models/users/decorators.py
+++ src/models/users/decorators.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from flask import session, request, redirect, url_for
 
-import src
+from src.app import app
 
 
 def requires_login(func):
@@ -19,7 +19,7 @@ def requires_admin_permissions(func):
     def decorated_function(*args, **kwargs):
         if 'email' not in session.keys() or session['email'] is None:
             return redirect(url_for('users.login_user', next=request.path))
-        if session['email'] not in src.config['ADMINS']:
+        if session['email'] not in app.config['ADMINS']:
             return redirect(url_for('users.login_user'))
         return func(*args, **kwargs)
 
